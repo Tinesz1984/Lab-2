f36a1c28086f59672dfc40daba6c099adf575bf8
Arina Shishkova <my.shishkova@gmail.com>
2025-10-21 20:33:17 +0300

    Add files via upload

---
+++ Доп_1.py
@@ -0,0 +1,20 @@
+#Теги - 13 стоб таблицы, 12 нужен нам
+from csv import reader 
+
+def search(table): 
+    list = []
+    for row in table:
+        stroke = row[12].replace('# ', '#').split('#')
+        for el in stroke: 
+            if el!='' and list.count(el)==0: 
+                list.append(el)
+    if len(list)==0: 
+        print('Ничего не найдено :(')
+    else: 
+        print(f'Количество различных тегов: {len(list)}')
+    for el in list: 
+        print(el)
+
+with open('books.csv', 'r') as csvfile: 
+    table = reader(csvfile, delimiter=';') 
+    search(table)
